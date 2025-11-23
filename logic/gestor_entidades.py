5aed3bce6bef4b1f5006c74bdb4c8c82b73e95a0
GabielOpeth <gabrielrivas515528@gmail.com>
2025-11-23 00:11:34 -0300

    Para gestionar entidades pue kskskk

---
+++ logic/gestor_entidades.py
@@ -0,0 +1,4 @@
+class MotorSimulacion:
+    def __init__(self, gestor_entidades, estado_simulacion):
+        self.gestor_entidades = gestor_entidades # <-- Asegúrate de que este objeto sea la instancia de GestorEntidades
+        # ... Mañana lo continuo kskskskskskskssk
