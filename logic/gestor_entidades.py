e3b8b69a449aa7c979c8acdb3625733bc17cb9d1
GabielOpeth <gabrielrivas515528@gmail.com>
2025-11-25 13:40:46 -0300

    Gestion identidades

--- logic/gestor_entidades.py
+++ logic/gestor_entidades.py
@@ -1,4 +1,4 @@
 class MotorSimulacion:
     def __init__(self, gestor_entidades, estado_simulacion):
-        self.gestor_entidades = gestor_entidades # <-- Asegúrate de que este objeto sea la instancia de GestorEntidades
+        self.gestor_entidades = gestor_entidades
         # ... Mañana lo continuo kskskskskskskssk
